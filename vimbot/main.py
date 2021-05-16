922c8d61a6d4fd75e346cd066998c23ee3a5beac
calee14 <caplee1000@gmail.com>
2021-05-16 01:52:20 -0700

    add event for on_message and allow commands to work coside

--- vimbot/main.py
+++ vimbot/main.py
@@ -7,6 +7,7 @@
 import discord
 from discord.ext import commands
 from secret import DISCORD_TOKEN # This is the token for our discord bot IMPORTANT
+import random
 
 # Here we're creating a client object for our bot
 # Here we can create functions for when the user calls the bot with a specific command
@@ -14,12 +15,37 @@ from secret import DISCORD_TOKEN # This is the token for our discord bot IMPORTA
 client = commands.Bot(command_prefix=".")
 token = DISCORD_TOKEN
 
-# This is the initialization event.
-# 
+# This is the initialization event for when the bot becomes active on the server
+# There are specific events such as on_ready() and on_member_join()
+# where the event will trigger a callback function that WE provide
 @client.event
 async def on_ready() :
     await client.change_presence(status = discord.Status.idle, activity = discord.Game("Listening to .help"))
     print("I am online")
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+
+@client.event
+async def on_message(message):
+    # This is an important if statement
+    # Guess what it does and why it's important
+    if message.author == client.user:
+        return
+
+    quotes = ['General Kenobi', 'Hello There', 'Hi']
+
+    if message.content == 'Hello there.':
+        response = random.choice(quotes)
+        await message.channel.send(response)
+
+    # Await the client to process the commands
+    # We wait because the commands are async functions and we don't know when they'll complete
+    await client.process_commands(message) # This will send the message ot the process commands function which will handle our command identifiers
 
 @client.command()
 async def ping(ctx) :
