d6dfab912c656447d87e09d47201b0c363860c6e
calee14 <caplee1000@gmail.com>
2021-05-16 01:27:08 -0700

    add vimbot base code

---
+++ vimbot/main.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+from secret import DISCORD_TOKEN
+
+
+client = commands.Bot(command_prefix=".")
+token = DISCORD_TOKEN
+
+@client.event
+async def on_ready() :
+    await client.change_presence(status = discord.Status.idle, activity = discord.Game("Listening to .help"))
+    print("I am online")
+
+@client.command()
+async def ping(ctx) :
+    await ctx.send(f"🏓 Pong with {str(round(client.latency, 2))}")
+
+@client.command(name="whoami")
+async def whoami(ctx) :
+    await ctx.send(f"You are {ctx.message.author.name}")
+
+@client.command()
+async def clear(ctx, amount=3) :
+    await ctx.channel.purge(limit=amount)
+
+
+client.run(token)
