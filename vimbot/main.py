564a3a8ef3b22eb33932814f65252c41b6c7c596
calee14 <caplee1000@gmail.com>
2021-05-16 01:36:11 -0700

    adding some comments to explain code

--- vimbot/main.py
+++ vimbot/main.py
@@ -1,11 +1,21 @@
+# TO MAKE A DISCORD APPLICATION AND BOT GO TO THIS LINK:
+# https://discord.com/developers/applications
+
+# Here we're making our imports from discord
+# Also getting our discord token which we have kept secret
+# Important to hide key or else bad stuff happens
 import discord
 from discord.ext import commands
-from secret import DISCORD_TOKEN
+from secret import DISCORD_TOKEN # This is the token for our discord bot IMPORTANT
 
-
+# Here we're creating a client object for our bot
+# Here we can create functions for when the user calls the bot with a specific command
+# as you can see we can call the bot with the prefix '.'
 client = commands.Bot(command_prefix=".")
 token = DISCORD_TOKEN
 
+# This is the initialization event.
+# 
 @client.event
 async def on_ready() :
     await client.change_presence(status = discord.Status.idle, activity = discord.Game("Listening to .help"))
